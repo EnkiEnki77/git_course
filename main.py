52a5bca803520a0b4e5d2bd681c3b74ddacfad13
EnkiEnki77 <layman212@gmail.com>
2025-03-27 03:58:47 -0400

    Learned about the basics of what git is capable of

---
+++ main.py
@@ -0,0 +1,31 @@
+# Git is the most popular VCS (version control system).
+# It allows you to keep a history of code changes
+# Revert mistakes in your code.
+# Collab with other devs
+# Make backups of your code
+# etc...
+
+# Git allows you to track code changes over time by author with a set of commands to search, manipulate,
+# and revert history.
+# Git creates a commit, a set of changes tied to an author, time of day, and other info.
+# As many commits as you want can be added to the main line
+# At any commit you can branch off and create new commits on the new line
+# Any branch can be merged into any other branch, including the main line
+# Commits can be squashed into one commit
+# Commit messages can be edited.
+# Commits can also be reverted
+
+
+# Check out the git manual with: man git
+# commands to navigate:
+# j/k: up/down
+# q: quit
+# d: half page down
+# u: half page up
+# /<term>: search for term
+# n: next search term
+# N: last search term
+
+
+
+# Git has two categories of commands
