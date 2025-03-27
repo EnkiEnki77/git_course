b5fdbe02fcf2cf31e6d5a2227eabbe23670347dd
remdu <layman212@gmail.com>
2025-03-27 11:25:15 -0400

    Learned how to add to config and see whats in it for both local and global

---
+++ config.py
@@ -0,0 +1,7 @@
+# To interact with the git config: git config
+# To add a configuration: --add
+# To store the configuration in your global config ~/.gitconfig: --global
+# To store the configuration in the local repo only: omit --global
+
+# To view your config: git config --list --local | --global
+# Or view it directly: cat .git/config (local) ~/.gitconfig (global)
