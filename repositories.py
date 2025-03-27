0e2bacb3fe2728570d9b8742ce0d3656c109c629
remdu <layman212@gmail.com>
2025-03-27 04:39:17 -0400

    Learned how to initialize a git repo

---
+++ repositories.py
@@ -0,0 +1,10 @@
+# The first step of any project is to create a repo
+# A git repo represents a single project
+
+# A repo is essentially a directory that contains a project and a hidden .git directory where git stores
+# all of it's internal tracking and versioning info for the project.
+
+
+# To initialize a git a repo, go into an empty directory and run git init. This creates a hidden .git
+# directory in your project directory
+
