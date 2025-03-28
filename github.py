5c9e3dc717992cee37e2808099c3ee7b8f85eea9
remdu <layman212@gmail.com>
2025-03-28 04:38:18 -0400

    Learned how to create a remote repo on github and connect it to our local repo

---
+++ github.py
@@ -0,0 +1,8 @@
+# Github is the most popular website for hosting remote git repositories.
+
+# It serves as a backup of all your code on the cloud in case you lose your computer
+# A central place to share your code and collaborate with others
+# A public portfolio for all your coding projects
+
+# In github you can create remote repo's hosted on their servers, and then initialize a local repo on
+# your machine and connect it to the remote.
