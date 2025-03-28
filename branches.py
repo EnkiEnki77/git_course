c11a733ef936d325db3663fc0d5c6d842668f406
remdu <layman212@gmail.com>
2025-03-27 21:51:43 -0400

    Learned about what branches are for

---
+++ branches.py
@@ -0,0 +1,15 @@
+# Branches allow you to keep track of different changes separately
+
+# You can work on features seeing how they work out, if you like the changes you can merge the branch
+# into the main line making the changes permanent, if not you can just delete the branch and go back to
+# the main line.
+
+# Branching is a way to essentially prototype changes before making them permanent.
+
+# A branch is just a named pointer to a specific commit. When you create a branch youre creating a new
+# pointer to a specific commit. The commit the branch points to (the most recent in the branch) is called
+# the tip of the branch
+
+# Because a branch is just a pointer to a commit they're cheap to create.
+
+# To check what branch you're on: git branch
